19be888e5a5dd3d524e75f65f90c5395882d0bf1
Adrián <79727938+clhore@users.noreply.github.com>
2022-05-25 10:14:37 +0200

    Update wifi-host.sh

--- wifi-host.sh
+++ wifi-host.sh
@@ -1,16 +1,16 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # Author: Adrián Luján Muñoz ( aka clhore )
 
 # Colours
-greenColour="\e[0;32m\033[1m"
-endColour="\033[0m\e[0m"
-redColour="\e[0;31m\033[1m"
-blueColour="\e[0;34m\033[1m"
-yellowColour="\e[0;33m\033[1m"
-purpleColour="\e[0;35m\033[1m"
-turquoiseColour="\e[0;36m\033[1m"
-grayColour="\e[0;37m\033[1m"
+readonly greenColour="\e[0;32m\033[1m"
+readonly endColour="\033[0m\e[0m"
+readonly redColour="\e[0;31m\033[1m"
+readonly blueColour="\e[0;34m\033[1m"
+readonly yellowColour="\e[0;33m\033[1m"
+readonly purpleColour="\e[0;35m\033[1m"
+readonly turquoiseColour="\e[0;36m\033[1m"
+readonly grayColour="\e[0;37m\033[1m"
 
 trap ctrl_c INT
 
